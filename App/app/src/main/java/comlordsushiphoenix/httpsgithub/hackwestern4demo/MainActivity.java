ea18a7874b3e4fed96c0b750973c8b21edd4d45a
Erik Christensen <erik.christensen108@gmail.com>
2017-11-17 15:16:03 -0500

    Made some buttons and method stubs

--- App/app/src/main/java/comlordsushiphoenix/httpsgithub/hackwestern4demo/MainActivity.java
+++ App/app/src/main/java/comlordsushiphoenix/httpsgithub/hackwestern4demo/MainActivity.java
@@ -2,6 +2,7 @@ package comlordsushiphoenix.httpsgithub.hackwestern4demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //Opens a page to make a new order
+    public void createOrder(View view){
+        //TODO
+
+    }
+
+    //Opens a page to show available orders
+    public void viewOrder(View view){
+        //TODO
+
+    }
+
 }
