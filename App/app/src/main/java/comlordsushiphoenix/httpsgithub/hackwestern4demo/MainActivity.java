8118b98acb4fb6533a4093b7942da60b0b62bdf8
melissatran <melissa.tran.x@hotmail.com>
2017-11-18 13:46:15 -0500

    Initial branch commit

--- App/app/src/main/java/comlordsushiphoenix/httpsgithub/hackwestern4demo/MainActivity.java
+++ App/app/src/main/java/comlordsushiphoenix/httpsgithub/hackwestern4demo/MainActivity.java
@@ -10,7 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 
     //Opens a page to make a new order
     public void createOrder(View view){
