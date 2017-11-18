6b15ca25762e92266a0a5a534727e278736608cd
melissatran <melissa.tran.x@hotmail.com>
2017-11-18 15:38:46 -0500

    changes

---
+++ DeliveryFrontEnd/app/src/main/java/com/western/melissatran/deliveryfrontend/MainActivity.java
@@ -0,0 +1,13 @@
+package com.western.melissatran.deliveryfrontend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading_page);
+    }
+}
